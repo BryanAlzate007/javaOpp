00e20cef7e61cb6693d0e9fdbf44d707cefd1c69
Bryan Alzate <bryanalzate007@gmail.com>
2023-03-11 23:42:54 -0500

    variables de tipo private como funcionan

--- Patient.java
+++ Patient.java
@@ -1,11 +1,12 @@
 public class Patient {
-    String name;
-    String email;
-    String address;
-    String phoneNumber;
-    String birthday;
-    Double weight,height;
-    String blood;
+    private String name;
+    private String email;
+    private String address;
+    private String phoneNumber;
+    private String birthday;
+    private Double weight;
+    private Double height;
+    private String blood;
 
     Patient(String name, String email){
         this.name = name;
